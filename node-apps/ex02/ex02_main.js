f900f1b978ceb0f7070b3a4d0571e1eef66c760a
Genilson <genisnilson@gmail.com>
2021-03-17 19:41:37 -0300

    Commit inicial

--- node-apps/ex02/ex02_main.js
+++
@@ -1,5 +0,0 @@
-const utils = require('./ex02_utils')
-
-let message = 'show de bola'
-
-console.log(utils.upperText(message))
--- node-apps/ex02/ex02_utils.js
+++
@@ -1,9 +0,0 @@
-function upper(text){
-    return text.toUpperCase()
-}
-
-function lower(text){
-    return text.toLowerCase();
-}
-
-module.exports = { upperText: upper, lowerText: lower }
